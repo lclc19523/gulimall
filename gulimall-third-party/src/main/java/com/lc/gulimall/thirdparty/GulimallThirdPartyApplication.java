e073af761b6540c49000ad46accf928b616da447
liucong <1957583058@qq.com>
2022-11-11 16:34:05 +0800

    项目初始构建-原生OSS第三方

---
+++ gulimall-third-party/src/main/java/com/lc/gulimall/thirdparty/GulimallThirdPartyApplication.java
@@ -0,0 +1,23 @@
+package com.lc.gulimall.thirdparty;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GulimallThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallThirdPartyApplication.class, args);
+    }
+
+}
